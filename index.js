1bf737ea3b07e6a1acc1592f8af2aaec691d4be3
ChrisSeiter <cseiter@gmail.com>
2021-03-21 21:55:38 -0400

    change HTML elements with javascript, with statement throwing errors, change manually

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+document.querySelector("main#main").remove();
+const newHeader = document.createElement("h1");
+//with newHeader
+//	.id = "victory";
+//	.className = "victory";
+//	.innerHTML = "Chris is the champion!";
+newHeader.id = "victory";
+newHeader.className = "victory";
+newHeader.innerHTML = "Chris is the champion!";
+
+
